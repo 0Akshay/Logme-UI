6f50f9bc77be143e1c1f8ac7bf1e85273c00eb7f
Akshay <akshaythacmp@gmail.com>
2026-03-08 16:40:18 +0530

    Make electron work

---
+++ electron/main.cjs
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 600
+  });
+  win.loadFile(path.join(__dirname, "../renderer/simple.html"));
+}
+
+app.whenReady().then(createWindow);
