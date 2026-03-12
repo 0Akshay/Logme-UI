d9c3c551877d0fef3be90a9d905e0e2826905608
Akshay <akshaythacmp@gmail.com>
2026-03-12 23:00:57 +0530

    Refactor code

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -9,10 +9,7 @@ inputField.addEventListener("keydown", function(event) {
 });
 
 setTimeout(() => {
-    const newAgentMessage = document.createElement("div");
-    newAgentMessage.textContent = "Hi! I am Logme. What did you work on today?";
-    newAgentMessage.classList.add("message", "agent-message");
-    messagesArea.appendChild(newAgentMessage);
+    addNewMessage("agent", "Hi! I am Logme. What would you like to log today?");
     
     messagesArea.scrollTo({
         top: messagesArea.scrollHeight - messagesArea.clientHeight,
@@ -25,23 +22,13 @@ async function handleSubmit() {
 
     if (text == "") return;
 
-    const newUserMessage = document.createElement("div");
-    newUserMessage.textContent = text;
-    newUserMessage.classList.add("message", "user-message");
-    messagesArea.appendChild(newUserMessage);
+    addNewMessage("user", text);
+
     history_messages.push({"role": "user", "content": text});
     
     inputField.value = "";
-    
-    // const newAgentMessage = document.createElement("div");
-    // newAgentMessage.textContent = "Hi! I am Logme. What did you work on today?";
-    // newAgentMessage.classList.add("message", "agent-message");
-    // messagesArea.appendChild(newAgentMessage);
 
-    messagesArea.scrollTo({
-        top: messagesArea.scrollHeight - messagesArea.clientHeight,
-        behavior: "smooth"
-    });
+    scrollToBottom();
     
     const response = await fetch("http://127.0.0.1:8000/logme", {
         method: "POST",
@@ -51,34 +38,39 @@ async function handleSubmit() {
         body: JSON.stringify({
             data: JSON.stringify(history_messages)
         })
-
-
     });
-
-    console.log(response);
     
     const data = await response.json();
     console.log(data);
-    const newAgentMessage0 = document.createElement("div");
-    newAgentMessage0.textContent = data.date + " " + data.project_name + " " + data.type + " " + data.description + " " + data.hours_spent;
-    newAgentMessage0.classList.add("message", "agent-message");
-    messagesArea.appendChild(newAgentMessage0);
-
-    const newAgentMessage = document.createElement("div");
-    newAgentMessage.textContent = data.followup_question;
-    newAgentMessage.classList.add("message", "agent-message");
-    messagesArea.appendChild(newAgentMessage);
-
     history_messages.push({"role": "assistant", "content": JSON.stringify(data)});
 
-    if (data.followup_question == "") {
+    // addNewMessage("agent", data.date + " " + data.project_name + " " + data.type + " " + data.description + " " + data.hours_spent);
+
+    const followupQuestion = data.followup_question;
+
+    if (followupQuestion) {
+        addNewMessage("agent", followupQuestion);
+    }
+    else {
         history_messages = []
+        addNewMessage("agent", "Saved.")
+        addNewMessage("agent", "What else did you work on?")
     }
 
+    scrollToBottom();
+}
+
+function addNewMessage(type, message) {
+    const newMessage = document.createElement("div");
+    newMessage.textContent = message;
+    newMessage.classList.add("message")
+    type === "user" ? newMessage.classList.add("user-message") : newMessage.classList.add("agent-message");
+    messagesArea.appendChild(newMessage);
+}
+
+function scrollToBottom() {
     messagesArea.scrollTo({
         top: messagesArea.scrollHeight - messagesArea.clientHeight,
         behavior: "smooth"
     });
-
-
 }
